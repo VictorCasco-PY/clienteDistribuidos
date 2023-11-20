b4aba84780e5ef64c7e50ad94b5be62619ba41aa
VictorCasco-PY <cascoalmada@gmail.com>
2023-11-20 17:36:01 -0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+createApp(App).mount("#app");
